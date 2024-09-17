0b22f422726f15d3714671b8f8bf51f744e95db7
Bautista Dev <13372233-Bautistadev@users.noreply.gitlab.com>
2024-09-17 12:40:35 -0300

    fix: correccioón de entidades

--- src/main/java/com/eldar/challenge/Entities/Abstract/Marca.java
+++ src/main/java/com/eldar/challenge/Entities/Abstract/Marca.java
@@ -1,17 +1,25 @@
 package com.eldar.challenge.Entities.Abstract;
 
 import com.eldar.challenge.Entities.Interfaces.Tasa;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
-@Data
-@Builder
+
+
 @ToString
 @AllArgsConstructor
 @Entity
 @Table(name="Marca")
 public abstract class Marca implements Tasa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre",nullable = false)
     private String nombre;
 
+    public Marca(String nombre) {
+        this.nombre = nombre;
+    }
 }
